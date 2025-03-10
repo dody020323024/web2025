e71bfb4103aa5d4b4ffb426cf8460dd2595eebde
dody020323024 <doddypr72@gmail.com>
2025-03-10 13:10:17 +0800

    membuat prodi.php

--- index.php
+++ index.php
@@ -7,5 +7,29 @@
 </head>
 <body>
     <h1>DATA MAHASISWA</h1>
+    <br>
+    
+    <table border ="1" cellspacing="0" cellpadding="5" >
+        <thead>
+           <th>No</th>
+           <th>NIM</th>
+           <th>NAMA</th>
+        </thead>
+
+        <tbody>
+    <tr>
+        <td>1</td>
+        <td>777</td>
+        <td>suanang</td>
+     </tr>
+        <br>
+
+    <tr>
+            <td>2</td>
+            <td>999</td>
+            <td>sapi kelinci</td>
+    </tr>
+        </tbody>
+         </table>
 </body>
 </html>
